314d8dd744ccd6514119c2ad8739d0bd4b02dc09
Scott Wolchok <swolchok@fb.com>
2018-11-13 20:29:26 -0800

    Accelerate mona parser
    Summary: Mona seems to frequently create non-fatal ParserError objects, and profiling says this is expensive. Let's short-circuit this behavior.
    
    Reviewed By: andrewcuneo, dshahidehpour
    
    Differential Revision: D13046345
    
    fbshipit-source-id: 5946e3ae2cc84ff5ff8d5cff9e1fdd6364c6469e

--- src/js/object-mona-parser/object-mona-parser.js
+++ src/js/object-mona-parser/object-mona-parser.js
@@ -12,6 +12,10 @@
 const objectSpec = require('./object-spec/object-spec-parser');
 const algebraicType = require('./algebraic-type/algebraic-type-parser');
 
+// Disable slow error path in mona --
+// https://github.com/zkat/mona-internals/blob/a50e3231b5ffd6fdc65f2af6df932258355d64b0/src/index.js#L73
+Error.captureStackTrace = function(a, b){};
+
 module.exports = {
   parseObjectSpec: objectSpec.parse,
   parseAlgebraicType: algebraicType.parse,
